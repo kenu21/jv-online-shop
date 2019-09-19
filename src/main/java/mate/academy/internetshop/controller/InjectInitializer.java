6ecef561d43b6b61e040b1517c8da7ec71b210f7
kenu21 <kenu21@gmail.com>
2019-09-19 12:42:16 +0300

    added hw-17-1 (#3)
    * added hw-17-1
    
    * changed level of logger warn to info

--- src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
@@ -4,15 +4,18 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import mate.academy.internetshop.annotations.Injector;
+import org.apache.log4j.Logger;
 
 public class InjectInitializer implements ServletContextListener {
+    private static final Logger logger = Logger.getLogger(InjectInitializer.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
-            System.out.println("Dependency injection started...");
+            logger.info("Dependency injection started...");
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
+            logger.fatal("Inject has failed" + e);
             throw new RuntimeException(e);
         }
     }
